33d40b2070a44b9eacae5f444650d5a59a2c4303
me <asp424@ya.ru>
2022-04-15 20:26:42 +0300

    New

--- app/src/main/java/com/example/weatherable/data/bluetooth/BluetoothSource.kt
+++ app/src/main/java/com/example/weatherable/data/bluetooth/BluetoothSource.kt
@@ -6,7 +6,6 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.os.Build
-import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.example.weatherable.data.room.bluetooth_db.BluetoothDataDao
 import com.example.weatherable.data.room.bluetooth_db.models.PressureModel
@@ -15,6 +14,7 @@ import com.example.weatherable.data.view_states.BluetoothResponse
 import com.example.weatherable.utilites.DEVICE
 import com.example.weatherable.utilites.UUID_VAL
 import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
@@ -22,6 +22,7 @@ import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.util.*
@@ -42,31 +43,26 @@ class BluetoothSource @Inject constructor(
     private val device by lazy { adapter.getRemoteDevice(DEVICE) }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    suspend fun runBluetooth(source: String): Flow<BluetoothResponse> = callbackFlow {
+    suspend fun runBluetooth(source: String) = callbackFlow {
         launch(Default) {
-            if (checkSelfPermission()) {
-                runCatching { adapter.enable() }.onSuccess {
-                    runCatching { socket?.close() }.onSuccess {
-                        runCatching { delay(1000L); socket = create }.onSuccess {
-                            runCatching { work(source) { trySend(it) } }
-                                .onFailure { stop }.error(this@callbackFlow)
-                        }.error(this@callbackFlow)
-                    }.error(this@callbackFlow)
-                }
-            }
-        }
+             task({ if (checkSelfPermission()) adapter.enable() },
+                    { task({ socket?.close() },
+                        { task({ delay(1000L); socket = create },
+                            { task({ work(source) { trySend(it) } }, {}, 1) }) }) })  }
         awaitClose { stop }
-    }
+    }.flowOn(IO)
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private inline fun ProducerScope<BluetoothResponse>.task(
+        run: () -> Unit, suc: () -> Unit, type: Int = 0
+    ) = runCatching { run() }.onSuccess { suc() }.onFailure { if (type == 1) stop; trySend(it.error) }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private inline fun ProducerScope<BluetoothResponse>.work(
         source: String,
         onSend: (BluetoothResponse) -> Unit
     ) {
-        var counter = 0;
-        var writeTemp = true;
-        var writePres = true
+        var counter = 0; var writeTemp = true; var writePres = true
         if (checkSelfPermission()) {
             socket?.connect(); success
             socket?.inputStream?.bufferedReader().use { reader ->
@@ -75,7 +71,9 @@ class BluetoothSource @Inject constructor(
                         if (isNotEmpty() && this != "nAn") {
                             when (counter) {
                                 0 -> {
-                                    if (writeTemp) { writeTemp(this); writeTemp = false }
+                                    if (writeTemp) {
+                                        writeTemp(this); writeTemp = false
+                                    }
                                     onSend(this.temp); counter = 1
                                 }
                                 1 -> {
@@ -96,7 +94,7 @@ class BluetoothSource @Inject constructor(
     private val ProducerScope<BluetoothResponse>.stop
         get() = runCatching { socket?.close() }.onSuccess {
             if (checkDisable && checkSelfPermission()) adapter.disable(); wait
-        }.error(this)
+        }
 
     private fun checkSelfPermission() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         ActivityCompat.checkSelfPermission(context, BLUETOOTH_CONNECT) != 0
@@ -114,9 +112,7 @@ class BluetoothSource @Inject constructor(
     )
 
     private val String.press get() = BluetoothResponse.Press(this)
-
     private val String.temp get() = BluetoothResponse.Temp(this)
-
     private val Throwable.error get() = BluetoothResponse.Error(message.toString())
 
     private val create
@@ -124,14 +120,11 @@ class BluetoothSource @Inject constructor(
             device.createRfcommSocketToServiceRecord(UUID.fromString(UUID_VAL)) else null
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private fun <T> Result<T>.error(scope: ProducerScope<BluetoothResponse>) =
-        onFailure { e -> scope.trySend(e.error) }
+    private val ProducerScope<BluetoothResponse>.success get() = trySend(BluetoothResponse.OnSuccess)
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private val ProducerScope<BluetoothResponse>.success get() = trySendBlocking(BluetoothResponse.OnSuccess)
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private val ProducerScope<BluetoothResponse>.wait get() = trySend(BluetoothResponse.Wait)
+    private val ProducerScope<BluetoothResponse>.wait
+        get() = trySend(BluetoothResponse.Wait)
 
     fun getAllTemps(): List<TempModel>? = bluetoothDataDao.getAllItemsTemp()
 
